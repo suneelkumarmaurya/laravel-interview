c5d398c4d7d9a28bc32466d5274feec5628ade8b
suneelkumarmaurya <suneelmaurya55@gmail.com>
2023-11-01 22:30:53 +0530

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Models\CustomerAddress;
+
+class CustomerController extends Controller
+{
+    public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:customers,email',
+            'phone'=>'required',
+            'Address'=>'required',
+            'Pincode'=> 'required',
+        ]) ;
+        Customer::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'phone'=>$request->phone,
+
+        ]);
+        $Address = new CustomerAddress();
+        $Address->Address = $request->Address;
+        $Address->Pincode = $request->Pincode;
+        $Address->save();
+
+        return back()->with('success','Customer added successfully');
+    }
+}
